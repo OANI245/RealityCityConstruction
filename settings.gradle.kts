ba241428aada5dbc2f41715639a6c82326248c8d
OANI245 <zu2ht@hotmail.com>
2026-05-13 09:06:06 +0800

    26.2 Snapshot 7 support

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,7 @@ stonecutter {
     create(rootProject) {
         // See https://stonecutter.kikugie.dev/wiki/start/#choosing-minecraft-versions
         versions("1.20.1", "1.21.1", "1.21.8", "1.21.10", "1.21.11")
-        versions("26.1", "26.2-snapshot-3").buildscript("build.unobf.gradle.kts")
+        versions("26.1", "26.2-snapshot-7").buildscript("build.unobf.gradle.kts")
         vcsVersion = "1.21.1"
     }
 }
